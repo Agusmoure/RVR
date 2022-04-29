87cc7d380d34ac129b079cda4a3de1042347848c
Unknown <amoure@ucm.es>
2022-04-29 12:01:56 +0200

    Class Socket

--- P2/rvr-release1.0/practica2.2/replicacion-chat/Socket.cc
+++ P2/rvr-release1.0/practica2.2/replicacion-chat/Socket.cc
@@ -6,7 +6,22 @@
 Socket::Socket(const char * address, const char * port):sd(-1)
 {
     //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
+    struct addrinfo *res;
+    struct addrinfo hints;
+    memset(&hints,0,sizeof(hints));
+    hints.ai_family=AF_INET;
+    hints.ai_socketype=SOCK_DGRAM;
+    hints.ai_protocol=17;
+    int rc= getaddrinfo(address, port,&hints,&res);
+    if(rc!=0){
+        //Si se produce fallo muestra cual es
+        std::cerr<<"getaddrinfo: "<<gai_sterror(rc)<<std::endl;
+    }
     //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+    sa=*res->ai_adr;
+    sa_len=sizeof(sa);
+    sd=socket(res->ai_family,res->ai_socketype,res->ai_prototcol);
+    freeadrinfo(res);
 }
 
 int Socket::recv(Serializable &obj, Socket * &sock)
@@ -36,7 +51,10 @@ int Socket::recv(Serializable &obj, Socket * &sock)
 int Socket::send(Serializable& obj, const Socket& sock)
 {
     //Serializar el objeto
+    obj.to_bin();
+
     //Enviar el objeto binario a sock usando el socket sd
+    return sendto(sd,obj.data(),obj.size(),0,&sock.sa,sock.sa_len);
 }
 
 bool operator== (const Socket &s1, const Socket &s2)
@@ -44,6 +62,13 @@ bool operator== (const Socket &s1, const Socket &s2)
     //Comparar los campos sin_family, sin_addr.s_addr y sin_port
     //de la estructura sockaddr_in de los Sockets s1 y s2
     //Retornar false si alguno difiere
+    sockaddr_in* s1_in=(struct sockaddr_in)&s1.sa;
+    sockaddr_in* s2_in=(struct sockaddr_in)&s2.sa;
+    bool equals=s1_in->sin_family==s2_in->sin_family
+    &&s1_in.sin_addr.s_addr==s2_in.sin_addr.s_addr
+    &&s1_in.sin_port==s2_in.sin_port;
+    
+    return equals;
 };
 
 std::ostream& operator<<(std::ostream& os, const Socket& s)
